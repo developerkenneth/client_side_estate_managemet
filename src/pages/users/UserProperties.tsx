bb6c5ce58a29816fa282b3e41fa75c8753b4b10e
developerkenneth <107970456+developerkenneth@users.noreply.github.com>
2026-06-11 17:17:22 +0100

    working on showing users project

--- src/pages/users/UserProperties.tsx
+++ src/pages/users/UserProperties.tsx
@@ -1,5 +1,28 @@
-export default function UserProperties(){
-    return(
-        <p>hello user</p>
-    )
+import { useEffect, useState } from "react";
+import { axiosAuth } from "../../utils/axiosAuth";
+import { useAuth } from "../../contexts/auth/authContext";
+
+export default function UserProperties() {
+  const [properties, setProperties] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const { user } = useAuth();
+  const userId = user.id;
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const response = await axiosAuth.get(`/properties?user_id=${userId}`);
+        const fetchedProperties = response.data.data;
+        setProperties(fetchProperties);
+      } catch (err) {
+        console.error(err.response?.data);
+        if (err.response.data) {
+        }
+      } finally {
+      }
+    };
+  }, []);
+  return <p>hello user</p>;
 }
